89f968aa88aec67c8986bd17a2106354ef1ad68a
Hamidreza.behzadi <hamidreza.behzadi@Admins-MacBook-Pro-3.local>
2024-08-16 12:30:13 +0330

    feat: add fa page

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,37 @@
-window.addEventListener("scroll", function (e) {
-  let navbar = document.querySelector("#navbar");
-  let logoFilter = document.querySelector("#logo");
+appLoad();
 
-  if (window.pageYOffset > 2) {
-    navbar.classList.add("scrolled");
-    logoFilter.classList.add("logo");
-  } else {
-    navbar.classList.remove("scrolled");
-    logoFilter.classList.remove("logo");
+function appLoad() {
+  let selectLang = document.querySelector("#lang");
+  if (selectLang) {
+    selectLang.addEventListener("change", changeLang);
+    if (localStorage.getItem("lang") === null) {
+      localStorage.setItem("lang", selectLang.value);
+    }
+
+    selectLang.value = localStorage.getItem("lang");
   }
-});
+
+  window.addEventListener("scroll", function (e) {
+    let navbar = document.querySelector("#navbar");
+    let logoFilter = document.querySelector("#logo");
+
+    if (window.pageYOffset > 2) {
+      navbar.classList.add("scrolled");
+      logoFilter.classList.add("logo");
+    } else {
+      navbar.classList.remove("scrolled");
+      logoFilter.classList.remove("logo");
+    }
+  });
+}
+
+function changeLang(e) {
+  let lang = e.target.value;
+  localStorage.setItem("lang", lang);
+
+  if (lang === "fa") {
+    window.location.replace("/index-fa.html");
+  } else {
+    window.location.replace("/index.html");
+  }
+}
