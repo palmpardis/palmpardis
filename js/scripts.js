e55f4b74bb738d17da8a670212fe36dd49a368d8
Hamidreza.behzadi <hamidreza.behzadi@Admins-MacBook-Pro-3.local>
2024-08-23 22:05:34 +0330

    feat: add emailjs

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,12 @@
 appLoad();
 
 function appLoad() {
+  emailjs.init({
+    publicKey: "r2IDO4f27YdTO0KRV",
+  });
+
   let selectLang = document.querySelector("#lang");
+  let form = document.querySelector("#form");
   if (selectLang) {
     selectLang.addEventListener("change", changeLang);
     if (localStorage.getItem("lang") === null) {
@@ -23,6 +28,10 @@ function appLoad() {
       logoFilter.classList.remove("logo");
     }
   });
+
+  if (form) {
+    form.addEventListener("submit", onSubmitForm);
+  }
 }
 
 function changeLang(e) {
@@ -34,4 +43,32 @@ function changeLang(e) {
   } else {
     window.location.replace("/index.html");
   }
+}
+
+function onSubmitForm(e) {
+  e.preventDefault();
+
+  form.submit.setAttribute("disabled", true);
+
+  emailjs.sendForm("service_s4t4stf", "template_1tg8otl", "#form").then(
+    (response) => {
+      if (document.documentElement.lang === "fa") {
+        alert("ایمیل با موفقیت ارسال شد");
+      } else {
+        alert("email successfully sent");
+      }
+
+      form.submit.removeAttribute("disabled");
+      form.reset();
+    },
+    (error) => {
+      if (document.documentElement.lang === "fa") {
+        alert("خطا در ارسال اطلاعات");
+      } else {
+        alert("FAILED...");
+      }
+
+      form.submit.removeAttribute("disabled");
+    }
+  );
 }
